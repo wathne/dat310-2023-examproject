d8674fef403374ae2ee9c8b4c90cb1b183fd34dd
magnusziem <magnus_ziem@outlook.com>
2023-05-09 09:48:45 +0200

    Oppdatert version

--- static/script.js
+++ static/script.js
@@ -1,18 +1,30 @@
-function NewThread() {
-    document.getElementById("NewThread").innerHTML = 
-    "<form action=''>" +
-    "<label for='title'>Thread title:</label><br>" +
-    "<input type='text' id='title' name='title' placeholder='New destination'><br><br>" +
-    "<label for='description'>Description:</label><br>" +
-    "<textarea type='text' id='description' name='description' rows='10' cols='81' placeholder='Descriptive paragraph...'></textarea><br><br>" +
-    "<label for='image'>Upload image:</label><br>" +
-    "<input type='file' id='image' name='image' accept='image/*'><br><br>" +
-    "<input type='submit' value='Post' onclick='NewThreadComment()'> "
-    "</form>";
-}
+let plus = false;
 
-function CloseNewThread() {
-    document.getElementById("NewThread").innerHTML = "";
+function NewThread() {
+    
+    plus = !plus;
+
+    if(plus) {
+        document.getElementById("NewThreadButton").innerHTML =
+        "<i id='NewThreadButton' class='fa-solid fa-minus' onclick='NewThread()'></i>";
+        document.getElementById("NewThread").innerHTML = 
+        "<form action=''>" +
+        "<label for='title'>Thread title:</label><br>" +
+        "<input type='text' id='title' name='title' placeholder='New destination'><br><br>" +
+        "<label for='description'>Description:</label><br>" +
+        "<textarea type='text' id='description' name='description' rows='10' cols='81' placeholder='Descriptive paragraph...'></textarea><br><br>" +
+        "<label for='image'>Upload image:</label><br>" +
+        "<input type='file' id='image' name='image' accept='image/*'><br><br>" +
+        "<input type='submit' value='Post' onclick='NewThreadComment()'> "
+        "</form>";
+    }
+    
+    else {
+        document.getElementById("NewThreadButton").innerHTML =
+        "<i id='NewThreadButton' class='fa-solid fa-plus' onclick='NewThread()'></i>"
+        document.getElementById("NewThread").innerHTML = "";
+    }
+
 }
 
 function NewThreadComment() {
